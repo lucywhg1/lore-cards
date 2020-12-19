eb0ed21e56c1697b0346eb07da0e156ecd772c38
Lucy Waggoner <lucywhg1@gmail.com>
2020-12-18 18:33:59 -0800

    Formats color as umber for titles and subtitles

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,11 +3,23 @@ import cx from "classnames";
 
 interface TitleProps {
   className?: string;
+  color?: string;
 }
 
-const Title: React.FC<TitleProps> = ({ children, className }) => {
+const Title: React.FC<TitleProps> = ({ children, className, color }) => {
   return (
-    <span className={cx("title", "is-uppercase", className)}>{children}</span>
+    <span
+      className={cx(
+        "title",
+        "is-small-caps",
+        {
+          [`has-text-${color}`]: color,
+        },
+        className
+      )}
+    >
+      {children}
+    </span>
   );
 };
 
